143ee431e9cb48c85ab9e30d34bfa2c6ff36658a
Oriol Vidal Teruel <38816232+uriivite@users.noreply.github.com>
2018-12-16 02:45:53 +0100

    Add files via upload

--- signatures.cc
+++ signatures.cc
@@ -9,20 +9,6 @@ using namespace std;
 
 #define INF 1000
 
-/*void imprimir_universal(map<int, string>& m) {
-  for (int i = 0; i < m.size(); ++i) {
-    cout << m[i] << endl;
-  }
-}
-
-void imprimir_sets(std::vector<set<string> >& v) {
-  for (auto i : v) {
-    for (auto s : i) {
-      cout << s << endl;
-    }
-  }
-}*/
-
 void imprimir_signatures(vector<vector<int> >& v) {
   for (int i = 0; i < v.size(); ++i){
     cout << "h" << i << ":	";
@@ -31,6 +17,22 @@ void imprimir_signatures(vector<vector<int> >& v) {
     }
     cout << endl;
   }
+}
+
+void jSeter(int k, string x, set<string>& s) {
+    for(int i = 0; i <= x.size()-k; ++i) {
+        string aux = "";
+        for(int j = i; j < i+k; ++j) {
+            aux.insert(aux.end(),x[j]);
+        }
+        s.insert(aux);
+    }
+}
+
+set<string> jUnion(const set<string>& a, const set<string>& b) {
+    set<string> x = a;
+    x.insert(b.begin(),b.end());
+    return x;
 }
 
 double jaccard(std::vector<std::vector<int> >& v) {
@@ -67,49 +69,17 @@ void init_signatures(vector<vector<int> >& v) {
 }
 
 int main() {
-  set<string> mA, mB;
-  cout << "Introducir textos a comparar:" << endl;
   string A, B;
   getline(cin, A);
   getline(cin, B);
-  cout << "\nTexto 1: " << A << "\n\nTexto 2: " << B << endl;
-  int k;
-  cout << "\nK: ";
-  cin >> k;
-  // Obtenim els diferents sets de cada text
-  for(int i = 0; i < A.size(); ++i) {
-		if (A.size() - i + 1 > k) {
-			string aux = "";
-			for (int j = i; j < i+k; ++j)
-				aux+=A[j];
-			mA.insert(aux);
-		}
-  }
-	for(int i = 0; i < B.size(); ++i) {
-		if (B.size() - i + 1 > k) {
-			string aux = "";
-			for (int j = i; j < i+k; ++j)
-				aux+=B[j];
-			mB.insert(aux);
-		}
-  }
-  // Generem el set universal
-  set<string> universal;
-  set<string>::iterator itA = mA.begin();
-  while (itA != mA.end()) {
-    universal.insert(*itA);
-		++itA;
-  }
-  set<string>::iterator itB = mB.begin();
-  while (itB != mB.end()) {
-    universal.insert(*itB);
-    ++itB;
-  }
+  int k; cin >> k;
+  set<string> mA, mB;
+  jSeter(k, A, mA);
+  jSeter(k, B, mB);
+  set<string> universal = jUnion(mA, mB);
   map<int, string> univ_hash = hash_shingles(universal);
-  //imprimir_universal(univ_hash);
   vector<set<string> > sets(2);
   sets[0] = mA; sets[1] = mB;
-  //imprimir_sets(sets);
   vector<vector<int> > signatures(100, vector<int>(sets.size())); // 100 permutacions
                                                         // tantes columnes com dels textos tinguem
   init_signatures(signatures);
@@ -123,6 +93,5 @@ int main() {
 				}
 		}
 	}
-	//imprimir_signatures(signatures);
   cout << jaccard(signatures) << endl;
 }
